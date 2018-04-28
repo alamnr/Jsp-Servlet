ca2f77638184e8dfe49ae2c4969a0e3665ccd272
alamnr <alamnr@gmail.com>
2018-04-28 11:36:02 +0600

    commit for understanding servlet

--- src/org/koushik/javabrains/SimpleServlet.java
+++ src/org/koushik/javabrains/SimpleServlet.java
@@ -3,25 +3,20 @@ package org.koushik.javabrains;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(description="A simple servlet", urlPatterns={"/SimpleServletPath"})
+@WebServlet(description="A simple servlet", urlPatterns={"/SimpleServlet"})
 public class SimpleServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 	
-	protected void doGet(HttpServletRequest request,HttpServletResponse response){
+	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
 		System.out.println("Hello from GET method.");
-		try {
-			PrintWriter writer = response.getWriter();
-			writer.println("<h3>Hello in HTML</h3>");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		response.getWriter().write("<h3> Hello from Servlet get method - on the web page</h3>");
 	}
 	
 
